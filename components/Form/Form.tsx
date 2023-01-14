b17e34f59756fbdfa77f2725f39c25bd525c2c80
aniloli42 <aniloli42@gmail.com>
2023-01-14 19:39:36 +0545

    fixed button disabling error

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -140,7 +140,7 @@ const Form: NextComponentType = () => {
       <button
         type="submit"
         disabled={
-          touched.examYear == undefined ? true : !isValid && isSubmitting
+          touched.examYear == undefined ? true : !isValid || isSubmitting
         }
         className="bg-gray-600 py-2 px-4 text-white rounded-sm hover:bg-gray-700 focus-visible:bg-gray-700 ring-gray-300 disabled:bg-gray-300 disabled:cursor-not-allowed"
       >
