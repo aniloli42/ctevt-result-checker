e24d48f3b8bb34f2a1e00e11034738a9aa5596c3
aniloli42 <aniloli42@gmail.com>
2023-01-14 23:01:46 +0545

    disable color changed

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -142,7 +142,7 @@ const Form: NextComponentType = () => {
         disabled={
           touched.examYear == undefined ? true : !isValid || isSubmitting
         }
-        className="bg-gray-600 py-2 px-4 text-white rounded-sm hover:bg-gray-700 focus-visible:bg-gray-700 ring-gray-300 disabled:bg-gray-300 disabled:cursor-not-allowed"
+        className="bg-gray-600 py-2 px-4 text-white rounded-sm hover:bg-gray-700 focus-visible:bg-gray-700 ring-gray-300 disabled:bg-gray-400 disabled:cursor-not-allowed"
       >
         {isSubmitting ? "Downloading..." : "Download PDF"}
       </button>
