06541dea52cda296d3777e438ba16e8ceebb6f71
aniloli42 <aniloli42@gmail.com>
2023-01-14 22:57:45 +0545

    condition button text

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -144,7 +144,7 @@ const Form: NextComponentType = () => {
         }
         className="bg-gray-600 py-2 px-4 text-white rounded-sm hover:bg-gray-700 focus-visible:bg-gray-700 ring-gray-300 disabled:bg-gray-300 disabled:cursor-not-allowed"
       >
-        Download PDF
+        {isSubmitting ? "Downloading..." : "Download PDF"}
       </button>
     </form>
   )
